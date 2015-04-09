49cbdd54e08275abbc735d2d9951a1cbd1729f8f
Grzesiek Wojcik <grzesiek.wojcik@op.pl>
2015-04-09 21:16:35 +0200

    while

---
+++ zgadnij_liczbe.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 20:18:33 2015
+
+@author: root
+"""
+
+import random
+
+print ("Wybierz liczbe z zakresu 1-100")
+print ("zadaniem komputera bedzie odgadniecie jaka to liczba")
+
+moja = int (input("Podaj liczbe "))
+y = 0
+c = 1
+lista = 0
+d = 1165465
+while y != moja:
+	y = random.randint(c,d)
+	print ("\nliczba kompa: ",y)
+	lista += 1
+	if y > moja:
+		print ("za duzo")
+		d = y
+		print ("przedzial: ",c,d)
+	elif y < moja:
+		print ("za malo")
+		c = y
+		print ("przedzial: ",c,d)
+	else:
+		print ("znalazlem")
+		print ("moja = ",moja)
+		print ("Kompa = ",y)
+		print ("w tylu ruchach znalazles: ",lista)
